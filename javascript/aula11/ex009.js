66e1ce8564023018e2eae58df56bd6d2a26febac
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-07-18 10:52:32 -0300

    aula 10

---
+++ javascript/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Voce é Brasileira!')
+}else{
+    console.log('Voce é Estrangeira!')
+}
--- javascript/index.js
+++
@@ -1,3 +0,0 @@
-console.log('Ola mundo');
-
- 
